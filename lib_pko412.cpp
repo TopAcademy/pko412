c41b5c1370f418e22119557eecf8a5f9bb760f03
Max BeslanPC <max@huzm.ru>
2025-11-23 19:12:04 +0300

    Class Point2d defined

--- lib_pko412.cpp
+++ lib_pko412.cpp
@@ -1,5 +1,11 @@
-﻿
+﻿#include "point2d.h"
+
 int main()
 {
+    top::Point2d p1(10, 10);
+    top::Point2d p2 = p1;
+    p2 = p1 + p2;
+    p1.print();
+    p2.print();
     return 0;
 }
--- point2d.h
+++ point2d.h
@@ -1,1 +1,68 @@
 #pragma once
+#include <iostream>
+
+namespace top
+{
+
+	class Point2d
+	{
+	private:
+		// Class data
+		int x;
+		int y;
+
+	public:
+
+		// Default constructor
+		Point2d() : x(0), y(0) {}
+		// Two-args constructor
+		Point2d(int x, int y) : x(x), y(y) {}
+		// Copy constructor
+		Point2d(const Point2d& r) : x(r.x), y(r.y) {};
+
+		// Display position
+		void print() const;
+		// Move point to new coordinates
+		void move(int, int);
+
+		// Operators
+		Point2d& operator=(const Point2d&);
+		Point2d operator+(const Point2d&) const;
+	};
+
+
+	/*
+	 * Class methods definition
+	 */
+
+
+	// Display position
+	void Point2d::print() const {
+		std::cout << "(" << x << "; " << y << ")\n";
+	}
+
+	// Move point to new coordinates
+	void Point2d::move(int new_x, int new_y) {
+		x = new_x;
+		y = new_y;
+	}
+
+
+	/*
+	 * Class operators definition
+	 */
+
+
+	// Operator =
+	Point2d& Point2d::operator=(const Point2d& right) {
+		x = right.x;
+		y = right.y;
+		return *this;
+	}
+
+	// Operator +
+	Point2d Point2d::operator+(const Point2d& right) const {
+		return Point2d(x + right.x, y + right.y);
+	}
+
+} // namespace
