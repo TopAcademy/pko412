2abcc832b2247d64d52e77b82e0ccf33a3bd78e0
Max BeslanPC <max@huzm.ru>
2026-01-26 12:05:00 +0300

    Derived class BDEvent added

--- CalendarEvent.h
+++ CalendarEvent.h
@@ -9,22 +9,39 @@ namespace top {
 	/// Base class for calendar events
 	class CalendarEvent
 	{
-	private:
-
+	protected:
 		Date event_date;
 		std::string comment;
 
 	public:
-
 		// Constructor setting event to passed date
 		CalendarEvent(Date d, std::string c) : event_date(d), comment(c) {}
 
-		// Print event date on the console
-		void display() {
+		// Pure vurtual function - `display` interface to implement in derived classes
+		virtual void display() {
 			std::cout << "Calendar Event:\n- Event date  : ";
 			event_date.display();
 			std::cout << "\n- Description : " << comment << std::endl;
 		}
 	};
+
+
+	// Brithday event class
+	class BDEvent : public CalendarEvent
+	{
+		std::string person_name;
+
+	public:
+		// Constructor
+		BDEvent(Date date, std::string comment, std::string name)
+			: CalendarEvent(date, comment), person_name(name) {}
+
+		// Display implementation for BD Event
+		void display() {
+			event_date.display();
+			std::cout << " : the birthday of person: " << person_name << "\nComment: " << comment << std::endl;
+		}
+	};
+
 }
 
--- lib_pko412.cpp
+++ lib_pko412.cpp
@@ -7,8 +7,13 @@ using namespace top;
 using namespace std;
 
 
+
 int main()
 {
-	CalendarEvent event1(Date(14, 1, 2026), "OOP Lesson");
+	CalendarEvent event1(Date(1, 1, 2026), "Comment1");
+	BDEvent event2(Date(2, 2, 2026), "Comment2", "Jet Li");
+
 	event1.display();
+	cout << endl;
+	event2.display();
 }
