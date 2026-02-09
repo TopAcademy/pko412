623f493a472f991af42876e88d9f52c1d961a37c
Maxim Huzmiev <max@huzm.ru>
2026-02-09 18:16:09 +0300

    Added throws in ReadFile.h

--- ReadFile.h
+++ ReadFile.h
@@ -2,7 +2,9 @@
 #pragma once
 #include <iostream>
 #include <fstream>
+#include <filesystem>
 #include "BaseFile.h"
+#include "stream_exceptions.h"
 
 // Derived class - Files Reader
 class ReadFile : public BaseFile
@@ -22,7 +24,16 @@ public:
 	}
 
 	void open(std::string path) override {
+		// Check if file exists
+		std::filesystem::path fname = path;
+		if (!std::filesystem::exists(fname)) {
+			throw not_found("File not found");
+		}
+		// Try to open the file
 		stream.open(path);
+		if (!stream.is_open()) {
+			throw std::exception("Common file open error");
+		}
 	}
 
 	void close() {
